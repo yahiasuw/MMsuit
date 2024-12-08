95a9e423da7d15e82a82f7b172b20d1e87726b54
yahiasuw <yahias@uw.edu>
2024-12-08 07:30:19 -0800

    version 0.0.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='MMsuit',
-    version='0.0.2',
+    version='0.0.1',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
